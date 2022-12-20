834ef8824bd19106c87f86495dbb207bb4f30dda
deepsb-git <b.dpk1729@gmail.com>
2022-12-20 20:37:29 +0530

    reverse an array with testcase on intellij

---
+++ src/test/java/com/sde/ReverseAnArrayIterativelyTest.java
@@ -0,0 +1,41 @@
+package com.sde;
+
+import com.sde.easy.ReverseAnArrayIteratively;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ReverseAnArrayIterativelyTest {
+
+    @Test
+    public void testReverse_emptyArray() {
+        int[] array = {};
+        ReverseAnArrayIteratively.reverse(array);
+        assertArrayEquals(new int[] {}, array);
+
+    }
+
+    @Test
+    public void testReverse_oneElementArray() {
+        int[] array= {1};
+        ReverseAnArrayIteratively.reverse(array);
+        assertArrayEquals(new int[] {1}, array);
+
+    }
+
+    @Test
+    public void testReverse_oddLengthArray() {
+        int[] array= {1,2,3};
+        ReverseAnArrayIteratively.reverse(array);
+        assertArrayEquals(new int[] {3,2,1}, array);
+
+    }
+
+    @Test
+    public void testReverse_evenLengthArray() {
+        int[] array= {1,2,3,4};
+        ReverseAnArrayIteratively.reverse(array);
+        assertArrayEquals(new int[] {4,3,2,1}, array);
+
+    }
+}
