e2a699ceb2807d8c9955ec7a9812e0597ecbcd23
deepsb-git <b.dpk1729@gmail.com>
2022-12-29 11:36:20 +0530

    updated

--- src/main/java/com/sde/easy/FactorialOfNumber.java
+++ src/main/java/com/sde/easy/FactorialOfNumber.java
@@ -1,15 +1,16 @@
 package com.sde.easy;
 
 public class FactorialOfNumber {
-    public static void main(String[] args){
-        int op=facto(5);
+    public static void main(String[] args) {
+        int op = facto(5);
         System.out.println("factorialOfNumber : " + op);
     }
-    static int facto(int n){
-        if(n<=1){
+
+    static int facto(int n) {
+        if (n <= 1) {
             return 1;
         }
-        return n*facto(n-1);
+        return n * facto(n - 1);
     }
 
 }
