b1e7f7978180ca8554d81fd7d5a0050f56de4b09
deepsb-git <b.dpk1729@gmail.com>
2023-01-12 22:17:32 +0530

    reverse of number initial commit

---
+++ src/main/java/com/sde/easy/ReverseNum.java
@@ -0,0 +1,15 @@
+package com.sde.easy;
+
+public class ReverseNum {
+    public static void main(String[] args){
+        rev(1234);
+        System.out.println("reverse of num : " + sum);
+    }
+    static int sum=0;
+    static void rev(int num){
+        if(num==0) return;
+        int rem=num%10;
+        sum=sum*10+rem;
+         rev(num/10);
+    }
+}
